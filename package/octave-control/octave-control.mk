54e7aef398e423097336e05ed40540961206ec1f
Alex Roberts <ARob109@gmail.com>
2022-04-26 20:12:15 -0500

    initial working state

--- package/octave-control/octave-control.mk
+++ package/octave-control/octave-control.mk
@@ -4,6 +4,7 @@ OCTAVE_CONTROL_SITE = http://hg.code.sf.net/p/octave/control
 OCTAVE_CONTROL_SITE_METHOD = hg
 OCTAVE_CONTROL_LICENSE = GPL-3.0+
 OCTAVE_CONTROL_LICENSE_FILES = COPYING
+OCTAVE_CONTROL_INSTALL_STAGING = YES
 
 OCTAVE_CONTROL_DEPENDENCIES = \
 		      octave
@@ -12,19 +13,32 @@ export INCLUDEDIR
 
 OCTAVE=$(HOST_DIR)/bin/octave
 
-define OCTAVE_CONTROL_PRE_BUILD_HOOKS
-	$(info "POST EXTRACT HOOKS")
-	cd $(@D)/src && autoreconf;
-	cd $(@D)/src && ./configure
+define OCTAVE_CONTROL_PRE_BUILD_HOOOKS
+	if [[ -d (@D)/src/sltmp ]]
+       	then
+		rmdir (@D)/src/sltmp
+	fi
 endef
 
 define OCTAVE_CONTROL_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
 endef
 
-define OCTAVE_CONTROL_INSTALL_CMDS
-	mkdir -p $(@D)/buildir
-	$(OCTAVE) --eval "pkg build $(@D)/buildir $(OCTAVE_CONTROL_SOURCE)"
+define OCTAVE_CONTROL_INSTALL_STAGING_CMDS
+	mkdir -p $(STAGING_DIR)/usr/share/octave/$(OCTAVE_CONTROL_VERSION)/inst
+	mkdir -p $(STAGING_DIR)/usr/share/octave/$(OCTAVE_CONTROL_VERSION)/src
+	mkdir -p $(STAGING_DIR)/usr/share/octave/$(OCTAVE_CONTROL_VERSION)/src/$(ARCH)
+	$(INSTALL) -D -m 0755 $(@D)/inst/*.m $(STAGING_DIR)/usr/share/octave/$(OCTAVE_CONTROL_VERSION)/inst
+	$(INSTALL) -D -m 0755 $(@D)/src/*.oct $(STAGING_DIR)/usr/share/octave/$(OCTAVE_CONTROL_VERSION)/src
+	$(INSTALL) -D -m 0755 $(@D)/src/*.cc $(STAGING_DIR)/usr/share/octave/$(OCTAVE_CONTROL_VERSION)/src
+	$(INSTALL) -D -m 0755 $(@D)/COPYING $(STAGING_DIR)/usr/share/octave/$(OCTAVE_CONTROL_VERSION)
+	$(INSTALL) -D -m 0755 $(@D)/DESCRIPTION $(STAGING_DIR)/usr/share/octave/$(OCTAVE_CONTROL_VERSION)
+	$(INSTALL) -D -m 0755 $(@D)/COPYING $(STAGING_DIR)/usr/share/octave/$(OCTAVE_CONTROL_VERSION)
+	$(INSTALL) -D -m 0755 $(@D)/INDEX $(STAGING_DIR)/usr/share/octave/$(OCTAVE_CONTROL_VERSION)
+endef
+
+define OCTAVE_CONTROL_INSTALL_TARGET_CMDS
+	tar -cvzf $(TARGET_DIR)/usr/share/octave/$(OCTAVE_CONTROL_SOURCE) -C $(STAGING_DIR)/usr/share/octave $(OCTAVE_CONTROL_VERSION)
 endef
 
 $(eval $(generic-package))
