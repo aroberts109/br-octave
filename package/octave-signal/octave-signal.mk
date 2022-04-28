b9349b3255a3cf48727f37b2a2d796f81f503ff3
Alex Roberts <ARob109@gmail.com>
2022-04-28 04:30:29 -0500

    Add Octave Signal package support

---
+++ package/octave-signal/octave-signal.mk
@@ -0,0 +1,39 @@
+OCTAVE_SIGNAL_VERSION = 1.4.2
+OCTAVE_SIGNAL_SOURCE = signal-$(OCTAVE_SIGNAL_VERSION).tar.gz
+OCTAVE_SIGNAL_SITE = http://hg.code.sf.net/p/octave/signal
+OCTAVE_SIGNAL_SITE_METHOD = hg
+OCTAVE_SIGNAL_LICENSE = GPL-3.0+
+OCTAVE_SIGNAL_LICENSE_FILES = COPYING
+OCTAVE_SIGNAL_INSTALL_STAGING = YES
+
+OCTAVE_SIGNAL_DEPENDENCIES = \
+		      octave
+
+INCLUDEDIR=$(STAGING_DIR)/usr/include
+export INCLUDEDIR
+
+define OCTAVE_SIGNAL_PRE_BUILD_HOOOKS
+endef
+
+define OCTAVE_SIGNAL_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
+endef
+
+define OCTAVE_SIGNAL_INSTALL_STAGING_CMDS
+	mkdir -p $(STAGING_DIR)/usr/share/octave/signal-$(OCTAVE_SIGNAL_VERSION)/inst
+	mkdir -p $(STAGING_DIR)/usr/share/octave/signal-$(OCTAVE_SIGNAL_VERSION)/src
+	mkdir -p $(STAGING_DIR)/usr/share/octave/signal-$(OCTAVE_SIGNAL_VERSION)/src/$(ARCH)
+	$(INSTALL) -D -m 0755 $(@D)/inst/*.m $(STAGING_DIR)/usr/share/octave/signal-$(OCTAVE_SIGNAL_VERSION)/inst
+	$(INSTALL) -D -m 0755 $(@D)/src/*.oct $(STAGING_DIR)/usr/share/octave/signal-$(OCTAVE_SIGNAL_VERSION)/src
+	$(INSTALL) -D -m 0755 $(@D)/src/*.cc $(STAGING_DIR)/usr/share/octave/signal-$(OCTAVE_SIGNAL_VERSION)/src
+	$(INSTALL) -D -m 0755 $(@D)/COPYING $(STAGING_DIR)/usr/share/octave/signal-$(OCTAVE_SIGNAL_VERSION)
+	$(INSTALL) -D -m 0755 $(@D)/DESCRIPTION $(STAGING_DIR)/usr/share/octave/signal-$(OCTAVE_SIGNAL_VERSION)
+	$(INSTALL) -D -m 0755 $(@D)/COPYING $(STAGING_DIR)/usr/share/octave/signal-$(OCTAVE_SIGNAL_VERSION)
+	$(INSTALL) -D -m 0755 $(@D)/INDEX $(STAGING_DIR)/usr/share/octave/signal-$(OCTAVE_SIGNAL_VERSION)
+endef
+
+define OCTAVE_SIGNAL_INSTALL_TARGET_CMDS
+	tar -cvzf $(TARGET_DIR)/usr/share/octave/signal-$(OCTAVE_SIGNAL_VERSION) -C $(STAGING_DIR)/usr/share/octave signal-$(OCTAVE_SIGNAL_VERSION)
+endef
+
+$(eval $(generic-package))
---
+++ package/octave-signal/test.m
@@ -0,0 +1,14 @@
+ pkg load signal
+ 
+ a = [ 1.0 -1.6216505 1.1102795 -0.4621741 0.2075552 -0.018756746 ];
+ white = rand(1,16384);
+ signal = detrend(filter(0.70181,a,white));
+ % frequency shift by modulating with exp(j.omega.t)
+ skewed = signal.*exp(2*pi*i*2/25*[1:16384]);
+ compat = pwelch ([]);
+ hold on;
+ pwelch(signal);
+ pwelch(skewed);
+ pwelch(signal,'shift','semilogy');
+ pwelch (compat);
+ hold off;
